f9b690c65d1ea1d75fc28b91af983f23b6e60b69
wl <991052286@qq.com>
2022-12-19 14:19:29 +0800

    修改Learn1的类描述为石头剪刀布

---
+++ src/Learn2.java
@@ -0,0 +1,18 @@
+public class Learn2 {
+    public static void main(String[] args) {
+        /**
+         * 求圆周率PI
+         */
+        double pi = 0;
+        for (double i=1;i<=1000;i++) {
+            // TODO
+            if(i==1){
+                pi = 4;
+            }else {
+                pi += 4*(i%2==0?-1/(i*2-1):1/(i*2-1));
+            }
+
+        }
+        System.out.println(pi);
+    }
+}
